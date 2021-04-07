f23d37632928f5b1da580d67f85138593bc28e60
radu <radustanciu841@yahoo.ro>
2021-04-07 17:35:48 +0300

    Create header Component

---
+++ src/components/Header.jsx
@@ -0,0 +1,13 @@
+// aceasta componenta este una simpla de tip functional Component pentru ilustrarea partii de header din aplicatie
+import React from 'react';
+
+function Header() {
+    return (
+        <div className='header'>
+            <img src="https://image.freepik.com/vecteurs-libre/logo-mobilier-elegant_23-2148451511.jpg" alt="random"/>
+            <h2>Articole pentru mobilierul unei case de vis</h2>
+        </div>
+    )
+}
+
+export default Header;
