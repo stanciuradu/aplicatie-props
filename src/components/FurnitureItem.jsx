60a05445b841abd3668d8b72cfcc609497b11843
radu <radustanciu841@yahoo.ro>
2021-04-07 17:35:08 +0300

    Create component Furniture

---
+++ src/components/FurnitureItem.jsx
@@ -0,0 +1,21 @@
+// esete o componenta simpla de tip function care prezinta un Item 
+import React from 'react';
+
+function FurnitureItem(props) {
+    // preiau prin destructuring cheile din obiectul props
+    const{name,price,model,src}=props;
+    return (
+        <div className='furniture-item'>
+            <img src={src} alt="random"/>
+            <div className='container'>
+                <p>{name}</p>
+                <p>{price}</p>
+                <p>{model}</p>
+            </div>
+           
+            
+        </div>
+    )
+}
+
+export default FurnitureItem;
